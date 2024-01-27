354c72eeed07f2f4f0fc993e39334596e32f0dc6
Kotokogotka <asics038@yandex.ru>
2024-01-27 17:43:20 +0300

    Добавлена обработка исключение в логирование

--- Logging/logging.py
+++ Logging/logging.py
@@ -16,6 +16,9 @@ def config_logging():
     logger = logging.getLogger('aiogram')
     logger.addHandler(file_handler)
 
+    # Обработка исключений
+    logger.exception = lambda msg: logger.error(f'EXCEPTION: {msg}')
+
 
 # Конфигурация логгировани
 config_logging()
