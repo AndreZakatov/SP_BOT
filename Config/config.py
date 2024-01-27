f87ca795bf8fc33b7c552cebd2229fec56eb52f8
Kotokogotka <asics038@yandex.ru>
2024-01-27 17:21:57 +0300

    Добавлен файл config.py где прописан доступ к чувствительным элементам бота

---
+++ Config/config.py
@@ -0,0 +1,27 @@
+import os
+from dataclasses import dataclass
+from environs import Env
+from dotenv import load_dotenv
+
+@dataclass
+class TgBot:
+    token: str  # Токен для доступа к телеграм боту
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot  # Телеграм бот
+
+
+# Функция загрузки конфигурации
+def load_env(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env("BOT_TOKEN")))
+
+# загрузка переменных окружение из .env
+load_dotenv()
+
+# Получение значений переменной ADMIN_IDS
+admin_ids_str = os.getenv('ADMIN_IDS').replace('[', '').replace(']', '').replace(',', '')
+allowed_user_ids = [int(admin_id) for admin_id in admin_ids_str.split()]
