fb389286ff5fdd5edaa7397c5f9cc458468c8de2
Kotokogotka <asics038@yandex.ru>
2024-01-27 17:37:24 +0300

    Добавление директории Logger и добавлени файла logger.py для логирования в файл sp_log.txt

---
+++ Logging/logging.py
@@ -0,0 +1,22 @@
+import logging
+
+
+def config_logging():
+    # Конфигурация логирования в консоль
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s | %(name)s | %(levelname)s | %(message)s',)
+
+    # Работа с файлом логирования
+    file_handler = logging.FileHandler('sp_log.txt')
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+    file_handler.setFormatter(formatter)
+
+    # Получение логгера в aiogram и добавление в файловый обработчик
+    logger = logging.getLogger('aiogram')
+    logger.addHandler(file_handler)
+
+
+# Конфигурация логгировани
+config_logging()
+
