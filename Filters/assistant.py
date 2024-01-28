08afbb3a398db3244a6105e94b0dbf3251341770
Kotokogotka <asics038@yandex.ru>
2024-01-28 22:00:38 +0300

    Добавлена файла assistant_parsing

--- Filters/assistant.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ChatMemberUpdated
-from aiogram.filters import BaseFilter
-from Config.config import allowed_assistant_ids
-
-class IsAssistant(BaseFilter):
-    async def __call__(self, event: ChatMemberUpdated) -> bool:
-        # Проверяем, является ли ID нового участника администратором
-        return event.new_chat_member.user.id in allowed_assistant_ids
