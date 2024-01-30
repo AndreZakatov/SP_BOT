f8414c813bdfe211eae01c5c7b9058524be69f45
Andrey <107883314+Kotokogotka@users.noreply.github.com>
2024-01-30 14:54:11 +0300

    добавлен файл с таблицами БД

--- Utils/sp_database.py
+++ Utils/sp_database.py
@@ -1,5 +1,5 @@
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Database:
@@ -8,7 +8,7 @@ class Database:
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
         # Включение поддержки внешних ключей
-        self.cursor.execute("PRAGMA foreigen_keys = ON;")
+        self.cursor.execute("PRAGMA foreign_keys = ON;")
 
     # Таблица администраторов
     def create_table_admins(self):
@@ -19,7 +19,7 @@ class Database:
         """)
         self.conn.commit()
 
-    # Таблица администраторов
+    # Таблица асистентов
     def create_table_assistant(self):
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS assistant (
@@ -28,20 +28,20 @@ class Database:
         """)
         self.conn.commit()
 
-    # Таблица администраторов
+    # Таблица подписчиков с подписками
     def create_table_subscriptions(self):
-        now = datetime.now()
-        current_date = now.strftime("%Y-%m-%d %H:%M:%S")
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS subscriptions (
                 user_id INTEGER,
                 status TEXT,
-                data_start DATETIME,
-                data_end DATETIME
+                date_start DATETIME,
+                date_end DATETIME,
+                FOREIGN KEY (user_id) REFERENCES users (telegram_id)
             )   
         """)
         self.conn.commit()
 
+    # Функция добавления даты в заданном формате
     def update_subscription(self, user_id, status, data_start):
         end_date = datetime.strptime(data_start, "%Y-%m-%d %H:%M:%S") + timedelta(days=10)
         end_date_str = end_date.strftime("%Y-%m-%d %H:%M:%S")
@@ -49,3 +49,40 @@ class Database:
             UPDATE subscriptions SET status = ?, data_end = ? WHERE user_id = ?
         """, (status, end_date_str, user_id))
         self.conn.commit()
+
+    # Таблица выполненных запросов на парсинг
+    def create_table_parsing(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS parsing (
+        id_parsing INTEGER PRIMARY KEY AUTOINCREMENT,
+        telegram_id INTEGER,
+        date DATETIME,
+        result TEXT,
+        FOREIGN KEY (telegram_id) REFERENCES users (telegram_id)
+        )
+        """)
+
+    # Таблица со статистикой
+    def create_table_statistics(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS statistics (
+        id_statistics INTEGER PRIMARY KEY AUTOINCREMENT,
+        date DATETIME,
+        total_users INTEGER,
+        active_users INTEGER,
+        new_users INTEGER,
+        requests_per_day INTEGER
+        )
+        """)
+
+    def create_all_table(self):
+        self.create_table_admins()
+        self.create_table_assistant()
+        self.create_table_subscriptions()
+        self.create_table_parsing()
+        self.create_table_statistics()
+
+
+if __name__ == "__main__":
+    db = Database('sp_database.db')
+    db.create_all_table()
